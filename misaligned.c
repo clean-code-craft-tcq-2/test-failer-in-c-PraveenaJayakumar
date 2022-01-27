0a0f6dc1f1aefcff35916a631b4d270a7d8ae13d
PraveenaJayakumar <90901057+PraveenaJayakumar@users.noreply.github.com>
2022-01-27 07:22:13 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -4,6 +4,8 @@
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
@@ -17,10 +19,16 @@ int numberOfMajorColors =
 const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
-const int MAX_COLORPAIR_NAME_CHARS = 16;
+
 int numberOfMinorColors =
     sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-const int MAX_COLORPAIR_NAME_CHARS = 16;
+
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+    sprintf(buffer, "%s %s",
+        MajorColorNames[colorPair->majorColor],
+        MinorColorNames[colorPair->minorColor]);
+}
 
 
 ColorPair GetColorFromPairNumber(int pairNumber) {
