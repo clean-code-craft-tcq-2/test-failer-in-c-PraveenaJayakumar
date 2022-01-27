6e960f3b9b58d4e4b90fe9fa9f74e21bf6f04fa2
PraveenaJayakumar <90901057+PraveenaJayakumar@users.noreply.github.com>
2022-01-27 07:25:12 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -60,7 +60,7 @@ int printColorMap() {
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
             printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
-            testNumberToPair(i * 5 + j, majorColor[i], minorColor[i]);
+            testNumberToPair(i * 5 + j, i, j);
         }
     }
     return i * j;
