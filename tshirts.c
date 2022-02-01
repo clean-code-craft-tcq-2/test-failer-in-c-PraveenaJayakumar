69f9461d8d20b6dc417eef2527cd9b1a18a05c71
PraveenaJayakumar <90901057+PraveenaJayakumar@users.noreply.github.com>
2022-02-01 19:57:32 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -17,7 +17,7 @@ int main() {
     assert(size(37) == 'S');
     assert(size(38) == 'S');
     assert(size(38) == 'M');
-    assert(size(42) == 'S');
+    assert(size(42) == 'M');
     assert(size(42) == 'M');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
