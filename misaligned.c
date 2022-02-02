f9c3dbc841b5c2e17b32ad2c98f37c5d34be5c54
PraveenaJayakumar <90901057+PraveenaJayakumar@users.noreply.github.com>
2022-02-02 11:36:12 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -41,7 +41,7 @@ void printColorMap() {
 int main() {
     printColorMap();
     testNumberToPair(0,  "White", "Blue");
-    testNumberToPair(1,  "White", "ORANGE");
+    
     printf("All is well (maybe!)\n");
     return 0;
 }
