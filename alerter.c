c1d4465fb7e21c8091ad57979f5b47b1819743d6
PraveenaJayakumar <90901057+PraveenaJayakumar@users.noreply.github.com>
2022-02-02 08:17:43 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -30,7 +30,7 @@ void alertInCelcius(float farenheit,int (*fpnetworkAlertStub)(float)) {
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+        alertFailureCount += 1;
     }
 }
 
